9fb233ad881506343c4fc2b51e0339e149d54a24
Jennifer Arias <jenniferarias414@gmail.com>
2024-04-09 13:03:52 -0500

    add to zero

--- Devmountain.js
+++ Devmountain.js
@@ -513,3 +513,38 @@ console.log(sortByProduct([23,2,3,4,5]))
 
 // ---------------------------------------------------
 
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+
+// for (let i = 0; i < array.length; i++) {
+//     for (let x = 0; x < array.length; x++) {
+//         if (array[i] + array[x] === 0) {
+//             console.log('true')
+//         } else {
+//             console.log('false')
+//         }
+//     }
+// }
+let value = false
+
+function addToZero(array) {
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        return true;
+      } 
+    }
+  }
+}
+}
+
+// console.log(value)
+// console.log(addToZero(array))
+value = addToZero(array)
+console.log(value)
+
+// ---------------------------------------------------
+
