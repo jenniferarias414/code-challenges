8d1b4ef29625f1df3dfbc12750042d86d9de6140
Jennifer Arias <jenniferarias414@gmail.com>
2024-05-14 08:36:45 -0500

    hasUniqueChars

--- Devmountain.js
+++ Devmountain.js
@@ -795,3 +795,17 @@ function decoder(code){
 
 // ---------------------------------------------------
 
+// Write your code below
+function hasUniqueChars(word) {
+  let uniqueChars = new Set([])
+  for (let i = 0; i < word.length; i++) {
+    uniqueChars.add(word[i])
+  }
+  return uniqueChars.size === word.length
+}
+
+console.log(hasUniqueChars('Lastly'))
+
+hasUniqueChars('Billy')
+
+// ---------------------------------------------------
